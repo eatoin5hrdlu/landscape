c60e1442e351058edcd9eb81d16633acd3a35680
Peter Reintjes <peter.reintjes@ncmls.org>
2018-01-03 16:55:59 -0500

    peak marked

--- landscape.py
+++ landscape.py
@@ -40,19 +40,31 @@ z =  3*(1 - x)**2 * np.exp(-x**2 - (y + 1)**2) \
    - 10*(x/5 - x**3 - y**5)*np.exp(-x**2 - y**2) \
    - 1./3*np.exp(-(x + 1)**2 - y**2) 
 
+maxx = 0
+maxy = 0
+maxz = 0
+for a in range(150):      # Find global peak
+    for b in range(150):
+        if (z[a][b] > maxz) :
+            maxz = z[a][b]
+            maxx = a
+            maxy = b
+        
 # Prepare starting locations
 n = 50
 vx = [0]*n
 vy = [0]*n
 vz = [0]*n
+rix = [0]*n
+riy = [0]*n
 rx = 0
 ry = 0
 random.seed(0)  # seed for reproducible 'random' numbers
 for i in range(n) :
-    rx = random.randrange(0,150)
-    ry = random.randrange(0,150)
-    vx[i] = x[rx][0]
-    vy[i] = y[0][ry]
+    rix[i] = random.randrange(0,150)
+    riy[i] = random.randrange(0,150)
+    vx[i] = x[rix[i]][0]
+    vy[i] = y[0][riy[i]]
 
 ls = LightSource(azdeg=0, altdeg=65)
 rgb = ls.shade(z, plt.cm.RdYlBu)
@@ -71,43 +83,45 @@ while True :
     if (count % 4 == 0) :
         if (edir) :
             if (elevation < 30) :
-                elevation = elevation + 1
+                elevation = elevation + 0.5
             else :
                 edir = not edir
         else :
             if (elevation > 10) :
-                elevation = elevation - 1
+                elevation = elevation - 0.5
             else :
                 edir = not edir
         if (adir) :
             if (azimuth < 40) :
-                azimuth = azimuth + 1
+                azimuth = azimuth + 0.5
             else :
                 adir = not adir
         else :
             if (azimuth > 10) :
-                azimuth = azimuth - 1
+                azimuth = azimuth - 0.5
             else :
                 adir = not adir
     ax.view_init(elevation,azimuth)
-    ax.dist = 4
+    ax.dist = 4.6
     for i in range(n) : # Crawl randomly ->(100,100) before assigning vz
-        if (rx < 100) :
-            dx = random.randint(-1,2)
+        if (rix[i] < max) :
+            dx = random.uniform(-0.1,0.2)
         else :
-            dx = random.randint(-2,1)
-        if (ry < 100) :
-            dy = random.randint(-1,2)
+            dx = random.uniform(-0.2,0.1)
+        if (riy[i] < 130) :
+            dy = random.uniform(-0.1,0.2)
         else :
-            dy = random.randint(-2,1)
-        rx = rx + dx
-        ry = ry + dy
-        vx[i] = x[rx][0]
-        vy[i] = y[0][ry]
-        vz[i] = z[rx][ry] + 1.0
+            dy = random.uniform(-0.2,0.1)
+        rix[i] = rix[i] + dx
+        riy[i] = riy[i] + dy
+        vx[i] = x[rix[i]][0]
+        vy[i] = y[0][riy[i]]
+        vz[i] = z[rix[i]][riy[i]] + 1.0
 
     ax.scatter(np.asarray(vx), np.asarray(vy), np.asarray(vz),
                c='r', marker='o')
+    ax.scatter(np.asarray([x[110][0]]),np.asarray([y[0][120]]),np.asarray([9]),
+               c='b', marker='D')
 
     surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=0,
                            antialiased=False, facecolors=rgb)
